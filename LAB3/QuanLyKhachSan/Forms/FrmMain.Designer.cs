7696250670132cbe9aab650fba7a47ef47dac58c
kim-lien <liennguyenthikim905@gmail.com>
2026-09-25 23:54:43 +0700

    Add files via upload

---
+++ LAB3/QuanLyKhachSan/Forms/FrmMain.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace QuanLyKhachSan.Forms
+{
+    partial class FrmMain
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        // Khai báo các Controls
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnDanhMuc;
+        private System.Windows.Forms.Button btnPhong;
+        private System.Windows.Forms.Button btnDatPhong;
+        private System.Windows.Forms.Button btnDichVu;
+        private System.Windows.Forms.Button btnTraPhong;
+        private System.Windows.Forms.Button btnThongKe;
+        private System.Windows.Forms.Button btnThoat;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnDanhMuc = new System.Windows.Forms.Button();
+            this.btnPhong = new System.Windows.Forms.Button();
+            this.btnDatPhong = new System.Windows.Forms.Button();
+            this.btnDichVu = new System.Windows.Forms.Button();
+            this.btnTraPhong = new System.Windows.Forms.Button();
+            this.btnThongKe = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(10, 45, 100);
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(784, 80);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "HỆ THỐNG QUẢN LÝ KHÁCH SẠN";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            // 
+            // btnDanhMuc
+            // 
+            this.btnDanhMuc.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.btnDanhMuc.Location = new System.Drawing.Point(40, 90);
+            this.btnDanhMuc.Name = "btnDanhMuc";
+            this.btnDanhMuc.Size = new System.Drawing.Size(220, 70);
+            this.btnDanhMuc.TabIndex = 1;
+            this.btnDanhMuc.Text = "Danh mục";
+            this.btnDanhMuc.UseVisualStyleBackColor = true;
+            this.btnDanhMuc.Click += new System.EventHandler(this.btnDanhMuc_Click);
+
+            // 
+            // btnPhong
+            // 
+            this.btnPhong.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.btnPhong.Location = new System.Drawing.Point(282, 90);
+            this.btnPhong.Name = "btnPhong";
+            this.btnPhong.Size = new System.Drawing.Size(220, 70);
+            this.btnPhong.TabIndex = 2;
+            this.btnPhong.Text = "Phòng - Tiện nghi";
+            this.btnPhong.UseVisualStyleBackColor = true;
+            this.btnPhong.Click += new System.EventHandler(this.btnPhong_Click);
+
+            // 
+            // btnDatPhong
+            // 
+            this.btnDatPhong.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.btnDatPhong.Location = new System.Drawing.Point(524, 90);
+            this.btnDatPhong.Name = "btnDatPhong";
+            this.btnDatPhong.Size = new System.Drawing.Size(220, 70);
+            this.btnDatPhong.TabIndex = 3;
+            this.btnDatPhong.Text = "Đặt / Nhận phòng";
+            this.btnDatPhong.UseVisualStyleBackColor = true;
+            this.btnDatPhong.Click += new System.EventHandler(this.btnDatPhong_Click);
+
+            // 
+            // btnDichVu
+            // 
+            this.btnDichVu.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.btnDichVu.Location = new System.Drawing.Point(40, 180);
+            this.btnDichVu.Name = "btnDichVu";
+            this.btnDichVu.Size = new System.Drawing.Size(220, 70);
+            this.btnDichVu.TabIndex = 4;
+            this.btnDichVu.Text = "Sử dụng dịch vụ";
+            this.btnDichVu.UseVisualStyleBackColor = true;
+            this.btnDichVu.Click += new System.EventHandler(this.btnDichVu_Click);
+
+            // 
+            // btnTraPhong
+            // 
+            this.btnTraPhong.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.btnTraPhong.Location = new System.Drawing.Point(282, 180);
+            this.btnTraPhong.Name = "btnTraPhong";
+            this.btnTraPhong.Size = new System.Drawing.Size(220, 70);
+            this.btnTraPhong.TabIndex = 5;
+            this.btnTraPhong.Text = "Trả phòng - Thanh toán";
+            this.btnTraPhong.UseVisualStyleBackColor = true;
+            this.btnTraPhong.Click += new System.EventHandler(this.btnTraPhong_Click);
+
+            // 
+            // btnThongKe
+            // 
+            this.btnThongKe.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.btnThongKe.Location = new System.Drawing.Point(524, 180);
+            this.btnThongKe.Name = "btnThongKe";
+            this.btnThongKe.Size = new System.Drawing.Size(220, 70);
+            this.btnThongKe.TabIndex = 6;
+            this.btnThongKe.Text = "Thống kê";
+            this.btnThongKe.UseVisualStyleBackColor = true;
+            this.btnThongKe.Click += new System.EventHandler(this.btnThongKe_Click);
+
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.btnThoat.Location = new System.Drawing.Point(282, 270);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(220, 70);
+            this.btnThoat.TabIndex = 7;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+
+            // 
+            // FrmMain
+            // 
+            this.ClientSize = new System.Drawing.Size(784, 381);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnThongKe);
+            this.Controls.Add(this.btnTraPhong);
+            this.Controls.Add(this.btnDichVu);
+            this.Controls.Add(this.btnDatPhong);
+            this.Controls.Add(this.btnPhong);
+            this.Controls.Add(this.btnDanhMuc);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quản lý khách sạn";
+            this.ResumeLayout(false);
+        }
+    }
+}
